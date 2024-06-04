279aeb9c3804cfd938a9798b9a3ae1e20908d673
aleksLiss <darklisik.darklisik@yandex.ru>
2024-06-04 21:16:35 +0300

    0.3. Тестирование исключений с junit [#219366 #499158]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FactorialTest {
 
     @Test
-    void calc() {
+    void whenCalcThenAssertThrows() {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> {
@@ -17,5 +17,13 @@ class FactorialTest {
         );
         String expected = "Number could not be less than 0";
         assertThat(exception.getMessage()).isEqualTo(expected);
+    }
+
+    @Test
+    void whenInput5Then120() {
+        int input = 5;
+        int result = Factorial.calc(5);
+        int expected = 120;
+        assertThat(result).isEqualTo(expected);
     }
 }
