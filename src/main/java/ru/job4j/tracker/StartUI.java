9cf8f708aba025a152a4b7a4d907e92e31d10739
aleksLiss <darklisik.darklisik@yandex.ru>
2024-06-04 21:59:59 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,13 @@
-    package ru.job4j.tracker;
+package ru.job4j.tracker;
 
-    import ru.job4j.tracker.action.*;
-    import ru.job4j.tracker.input.ConsoleInput;
-    import ru.job4j.tracker.input.Input;
-    import ru.job4j.tracker.input.ValidateInput;
-    import ru.job4j.tracker.output.ConsoleOutput;
-    import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.action.*;
+import ru.job4j.tracker.input.ConsoleInput;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.ConsoleOutput;
+import ru.job4j.tracker.output.Output;
 
-    public class StartUI {
+public class StartUI {
     private final Output output;
 
     public StartUI(Output output) {
@@ -37,7 +37,7 @@
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ValidateInput();
+        Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output),
