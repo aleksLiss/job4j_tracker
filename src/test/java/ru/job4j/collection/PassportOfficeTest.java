d874e5d37bc60218d690106797ab5e7ef780a360
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-05 21:51:52 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    void whenTestAddMethodThenReturnFalse() {
+        Citizen citizenOne = new Citizen("e145qq", "Ivan Ivanov");
+        Citizen citizenTwo = new Citizen("e145qq", "Ivan Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizenOne);
+        assertThat(office.add(citizenTwo)).isFalse();
+    }
+}
