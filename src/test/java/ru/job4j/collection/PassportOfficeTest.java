809710399731c7573697b03502e2aec9a7627c45
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-05 21:55:36 +0300

    1. Паспорт и жители [#212656 #503948]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -1,8 +1,8 @@
 package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PassportOfficeTest {
     @Test
