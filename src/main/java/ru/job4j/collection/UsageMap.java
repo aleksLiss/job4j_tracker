2ee74b6a3547c4ab5d3c79e608ce0906cdcf862e
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-06 07:28:27 +0300

    0. Map, HashMap [#212652 #503941]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,11 @@ import java.util.Map;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("aleksei@mail.ru", "PupkinIvanIvanovish");
+        map.put("aleksei@mail.ru", "PupkinIvanIvanovich");
+        map.put("ivanovich@gmail.com", "IvanovIvanIvanovich");
+        map.put("vova64@yandex.ru", "VovkinVovaNikolaevich");
+        map.put("vika44@yandex.ru", "IvanovaVikaViktorovna");
+        map.put("aleksei@mail.ru", "PupkinIvanIvanovich");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey());
         }
