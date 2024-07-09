afac1b433920068c4dea63b528368e7a28659fec
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-09 13:27:33 +0300

    3. Отсортировать департаменты [#34352 #504235]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -1,26 +1,16 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        List<String> leftArr = new ArrayList<>(List.of(left.split("/")));
-        List<String> rightArr = new ArrayList<>(List.of(right.split("/")));
-        int size = Math.min(leftArr.size(), rightArr.size());
-        int result;
-        for (int i = 0; i < size; i++) {
-            if (i == 0) {
-                result = rightArr.get(i).compareTo(leftArr.get(i));
-            } else {
-                result = leftArr.get(i).compareTo(rightArr.get(i));
-            }
-            if (result != 0) {
-                return result;
-            }
-        }
-        return Integer.compare(leftArr.size(), rightArr.size());
+        String[] leftArr = left.split("/", 2);
+        String[] rightArr = right.split("/", 2);
+        return (rightArr[0].compareTo(leftArr[0]) != 0)
+                ? rightArr[0].compareTo(leftArr[0])
+                : (leftArr[leftArr.length - 1].compareTo(rightArr[rightArr.length - 1]) != 0)
+                ? leftArr[leftArr.length - 1].compareTo(rightArr[rightArr.length - 1])
+                : Integer.compare(leftArr.length, rightArr.length);
     }
 }
