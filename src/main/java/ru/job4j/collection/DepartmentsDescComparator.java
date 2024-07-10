24e4d1d51982f9a01b435ee5efedea59dbfdbbed
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-10 12:45:09 +0300

    3. Отсортировать департаменты [#34352 #504235]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -7,10 +7,9 @@ public class DepartmentsDescComparator implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftArr = left.split("/", 2);
         String[] rightArr = right.split("/", 2);
-        return (rightArr[0].compareTo(leftArr[0]) != 0)
-                ? rightArr[0].compareTo(leftArr[0])
-                : (leftArr[leftArr.length - 1].compareTo(rightArr[rightArr.length - 1]) != 0)
-                ? leftArr[leftArr.length - 1].compareTo(rightArr[rightArr.length - 1])
-                : Integer.compare(leftArr.length, rightArr.length);
+        int result = rightArr[0].compareTo(leftArr[0]);
+        return result != 0
+                ? result
+                : leftArr[leftArr.length - 1].compareTo(rightArr[rightArr.length - 1]);
     }
 }
