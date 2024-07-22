a098855a6293c1b0ce11b27da2bfab33bb874fef
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-22 14:29:30 +0300

    10. Преобразование матрицы чисел в список чисел. FlatMap. [#110228]

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -1,8 +1,6 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
