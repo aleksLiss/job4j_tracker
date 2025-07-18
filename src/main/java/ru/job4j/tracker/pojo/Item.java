d949ddeb55b3544b25ef828d8954eefb47ee90f5
aleks <darklissik.darklissik@gmail.com>
2025-07-18 12:58:26 +0300

    1. Lombok. Настройка IDEA [#504888 #548615]

--- src/main/java/ru/job4j/tracker/pojo/Item.java
+++ src/main/java/ru/job4j/tracker/pojo/Item.java
@@ -17,7 +17,7 @@ public class Item {
 
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+    private LocalDateTime created = LocalDateTime.now().withNano(0);
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item(String name) {
