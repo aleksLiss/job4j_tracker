e8a15ef9f78707aa13c9974445eebc6ab03a97b6
aleks <darklissik.darklissik@gmail.com>
2025-07-21 20:16:52 +0300

    1. Lombok. Настройка IDEA [#504888 #548615]

--- src/main/java/ru/job4j/tracker/pojo/Item.java
+++ src/main/java/ru/job4j/tracker/pojo/Item.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 @EqualsAndHashCode(of = {"name"})
 @NoArgsConstructor
 public class Item {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
